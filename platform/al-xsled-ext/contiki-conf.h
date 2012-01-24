fe6793bd29c45e4793c8d7e5c5b6f18ba2f3ba29
Tim Rule <trule.github@nym.hush.com>
2012-01-24 21:23:34 +0100

    Add definitions to satisfy LEDS api
    Map these LEDs to a spare pin .. and ignore.
    
    Signed-off-by: Tim Rule <trule.github@nym.hush.com>

--- platform/al-xsled-ext/contiki-conf.h
+++ platform/al-xsled-ext/contiki-conf.h
@@ -32,6 +32,11 @@
 #define LEDS_CONF_ALL (LED1 | LED2 | LED3 | LED4)
 #define LED_STATUS LED1
 #define LED_ALERT LED2
+/* The following defs satisfy the LEDS api, and do nothing. */
+#define LEDS_GREEN PIN3_bm
+#define LEDS_YELLOW PIN3_bm
+#define LEDS_RED PIN3_bm
+#define LEDS_BLUE PIN3_bm
 
 /* Watchdog */
 #define WATCHDOG_CONF_TIMEOUT WDT_PER_8KCLK_gc
